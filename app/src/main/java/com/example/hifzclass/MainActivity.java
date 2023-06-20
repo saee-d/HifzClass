ca801a467d416f853c0e5b527070af8b56e3ad54
saee-d <130387033+saee-d@users.noreply.github.com>
2023-06-20 15:18:57 +0500

    fourth commit

--- app/src/main/java/com/example/hifzclass/MainActivity.java
+++ app/src/main/java/com/example/hifzclass/MainActivity.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,5 +45,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public static class EnrollStudent extends AppCompatActivity {
+        EditText name, age, rollno;
+        Button enroll;
+        TextView textView;
+
+        StudentsDbHelper studentsDbHelper;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_enroll_student);
+
+            Intent intent = getIntent();
+
+            studentsDbHelper = new StudentsDbHelper(this);
+
+            name = findViewById(R.id.name);
+            age = findViewById(R.id.age);
+            enroll = findViewById(R.id.enrollStudent);
+            rollno = findViewById(R.id.rollno);
+            textView = findViewById(R.id.textView3);
+
+            enroll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    String n = name.getText().toString();
+                    String a = age.getText().toString();
+                    String r = rollno.getText().toString();
+
+                    Student student = new Student(Integer.parseInt(r), n, a, null);
+
+                    if(n == null || a == null || r == null){
+                        // Add Toast
+                    }
+                    else {
+                        studentsDbHelper.insertStudent(student);
+                    }
+                }
+            });
+
+
+
+
+
+
+
+
+        }
     }
 }
